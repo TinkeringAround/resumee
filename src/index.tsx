f8b6f94ac2baf66cb70be1a2c03ea34d8b2f2b83
Thomas Maier <t.maier@gingco.net>
2019-09-16 14:34:14 +0200

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { FC } from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./utility/serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Styles
+import "./index.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+//=========================================================
+const App: FC = () => {
+  return <div className="App"></div>;
+};
+
+//=========================================================
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
